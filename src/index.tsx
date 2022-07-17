d0e998e3e7ed9f3f0a1705dc3e3c81a06db97a73
pwbear1230 <timewornbn@gmail.com>
2022-07-17 07:03:25 -0300

    dep: add bootstrap5 to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../src/style.scss';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
